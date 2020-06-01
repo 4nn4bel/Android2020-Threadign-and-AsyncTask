746930d0eac587b6c4c9539b7828527757f7aeeb
Meisam <Meisam@meiCode.org>
2020-06-02 02:08:59 +0400

    The source code for the video called 'Threading and AsyncTask' in the Android App Development Master Class for 2020

---
+++ app/src/main/java/org/meicode/threadingandasynctask/ExampleAsyncTask.java
@@ -0,0 +1,18 @@
+package org.meicode.threadingandasynctask;
+
+import android.os.AsyncTask;
+import android.os.SystemClock;
+import android.util.Log;
+
+public class ExampleAsyncTask extends AsyncTask<Integer, Void, Void> {
+    private static final String TAG = "ExampleAsyncTask";
+
+    @Override
+    protected Void doInBackground(Integer... integers) {
+        for (int i=0; i<integers[0]; i++) {
+            Log.d(TAG, "doInBackground: i was: " + i);
+            SystemClock.sleep(1000);
+        }
+        return null;
+    }
+}
